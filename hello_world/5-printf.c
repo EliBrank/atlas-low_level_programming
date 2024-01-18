cee07db739ee64d2659384ec5e96fa4986451676
EliBrank <eliasbrinkman@gmail.com>
2024-01-18 19:35:32 +0000

    fix syntax for betty

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - displays the text with proper grammar, but the outcome is a piece of art, followed by a newline
+ * main - displays with proper grammar, but the outcome is a piece of art,
  *
  * Return: 0 (always success)
  */
 
 int main(void)
 {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
