176844da90f3333267478c3e3e381ba811d82e1d
EliBrank <eliasbrinkman@gmail.com>
2024-01-18 23:08:57 +0000

    update no q,e alphabet script

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+
+/**
+ * main - prints alphabet except for "q, e"
+ *
+ * Return: 0 (always success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
