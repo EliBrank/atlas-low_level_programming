e83f412227e49f7df92d3a5aca25c9885737c60e
EliBrank <eliasbrinkman@gmail.com>
2024-01-18 22:29:56 +0000

    update alphabet script

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,24 @@
 #include <stdio.h>
+
+/**
+ * main - prints alphabet in lower and uppercase
+ *
+ * Return: 0 (always success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
