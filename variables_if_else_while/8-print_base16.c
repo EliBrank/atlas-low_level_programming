d5a51e1c1c170904c04838e7c9021936dc0ff99c
EliBrank <eliasbrinkman@gmail.com>
2024-01-19 05:41:54 +0000

    add preliminary updates to base16 script

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+/**
+ * main - prints single-digit base16 numbers
+ *
+ * Return: 0 (always success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
