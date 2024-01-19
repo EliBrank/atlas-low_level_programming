5a62e167bd3238a2fd950734623cb9fd79e39b57
EliBrank <eliasbrinkman@gmail.com>
2024-01-19 04:33:41 +0000

    update z-a putchar script

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+/**
+ * main - prints alphabet z-a
+ *
+ * Return: 0 (always success)
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
