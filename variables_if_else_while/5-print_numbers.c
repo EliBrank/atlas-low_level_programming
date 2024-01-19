041b9666292458e123efdb92a89d181b137f2d58
EliBrank <eliasbrinkman@gmail.com>
2024-01-19 03:48:18 +0000

    update print numbers script

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+/**
+ * main - prints number 0-9
+ *
+ * Return: 0 (always success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
