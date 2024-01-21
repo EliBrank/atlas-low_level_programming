414b653741504c75f2ff04e92f3466eec49c785b
EliBrank <eliasbrinkman@gmail.com>
2024-01-21 19:56:09 +0000

    add main.h file

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,5 +1,22 @@
+#include "_putchar.c"
+
 /**
+ * main - prints "_putchar"
  *
- *
- *
+ * Return: 0 (always success)
  */
+
+int main(void)
+{
+	char *s;
+	int i;
+
+	s = "_putchar";
+	for (i = 0; i != '\0'; i++)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
