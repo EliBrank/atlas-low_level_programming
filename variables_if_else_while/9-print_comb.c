0997dd93fc2cff74c5601719bd6242bac1596ecc
EliBrank <eliasbrinkman@gmail.com>
2024-01-21 17:18:49 +0000

    update single digit combo script

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+
+/**
+ * main - prints all combinations of single-digit nums 
+ *
+ * Return: 0 (always success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
