c56bf0cf32c3772deffa622e2739dbdf0b8c8f40
EliBrank <eliasbrinkman@gmail.com>
2024-01-22 16:30:19 +0000

    try updating abs function

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _abs - prints '+' for positive input, '-' for negative, 0 for 0
+ * @n: checked num
+ *
+ * Return: 1 if letter, else 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		(n * -1);
+		_putchar(n);
+
+		return (0);
+	}
+	else
+	{
+		_putchar(n);
+
+		return (0);
+	}
+
+	return (0);
+}
