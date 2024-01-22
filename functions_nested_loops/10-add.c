a9fee07172b5e3d335c0f614736c1c1d36ae3c40
EliBrank <eliasbrinkman@gmail.com>
2024-01-22 20:49:32 +0000

    update add func

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers together
+ * @x: first integer
+ * @y: second integer
+ *
+ * Return: sum of both numbers
+ */
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+
+	return (sum);
+}
