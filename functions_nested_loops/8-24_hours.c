eab4bc38cdcf9069f61eecf512ff086a29fc1f22
EliBrank <eliasbrinkman@gmail.com>
2024-01-22 18:26:27 +0000

    fix for loop in 24 func

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,9 +13,9 @@ void jack_bauer(void)
 
 	for (h1 = 0; h1 <= 2; h1++)
 	{
-		for (h2 = 0; h2 < 4; h2++)
+		for (h2 = 0; h2 < 5; h2++)
 		{
-			for (m1 = 0; m1 < 5; m1++)
+			for (m1 = 0; m1 < 6; m1++)
 			{
 				for (m2 = 0; m2 < 10; m2++)
 				{
