bdbb782ea16229e29120386209445adf22770aae
EliBrank <eliasbrinkman@gmail.com>
2024-01-22 18:19:33 +0000

    try updating 24 func

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,17 +1,24 @@
 #include "main.h"
 
 /**
- * print_last_digit - prints last digit of input num
- * @n: checked num
- *
- * Return: n
+ * jack_bauer - prints every minute in 24 hour format
  */
 
-int print_last_digit(int n)
+void jack_bauer(void)
 {
-	int last_digit;
+	int h1;
+	int h2;
+	int m1;
+	int m2;
 
-	last_digit = (n % 10);
-
-	return (last_digit);
+	for (h1 = 0; h1 <= 2; h1++)
+	{
+		for (h2 = 0; h2 < 4; h2++)
+		{
+			for (m1 = 0; m1 < 5; m1++)
+			{
+			for (m2 = 0; m2 < 10; m2++)
+			}
+		}
+	}
 }
