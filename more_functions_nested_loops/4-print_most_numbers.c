8de27313601ce28a376bdd5ec9cef562e8676664
EliBrank <eliasbrinkman@gmail.com>
2024-01-24 19:39:08 +0000

    change num printer to while loop, init i

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -6,13 +6,14 @@
 
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
 	if (i != 2 && i != 4)
 	{
-		for (i = 0; i < 10; i++)
+		while (i < 10)
 		{
 			_putchar(i + '0');
+			i++;
 		}
 	}
 	_putchar('\n');
