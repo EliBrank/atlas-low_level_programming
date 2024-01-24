cfa83415efba26135c21caa9034c51c557daf3e5
EliBrank <eliasbrinkman@gmail.com>
2024-01-24 19:25:33 +0000

    update mul func

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies 2 integers
+ * @a: multiplicand
+ * @b: multiplier
+ *
+ * Return: product of 2 input nums
+ */
+
+int mul(int a, int b)
+{
+	int product = a * b;
+
+	return (product);
+}
