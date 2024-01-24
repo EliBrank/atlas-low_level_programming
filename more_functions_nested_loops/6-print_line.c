ac261b14481a9ff99df23a516e50f257276005de
EliBrank <eliasbrinkman@gmail.com>
2024-01-24 20:41:42 +0000

    update print line func

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - draws line of underscores
+ * @n: number of times _ is printed
+ */
+
+void print_line(int n)
+{
+	int p = 0;
+
+	if (n > 0)
+	{
+		while (p <= n)
+		{
+			_putchar('_');
+			p++;
+		}
+	}
+	_putchar('\n');
+}
