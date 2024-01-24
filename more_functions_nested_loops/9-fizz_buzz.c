cda3e36acbf8edf49c38fbcc7c43c4f527c65c9e
EliBrank <eliasbrinkman@gmail.com>
2024-01-24 22:10:53 +0000

    update fizzbuzz function

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints 1-100, following FizzBuzz conventions
+ */
+
+void main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	putchar('\n');
+}
+
