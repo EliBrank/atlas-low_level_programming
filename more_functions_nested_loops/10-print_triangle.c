c17b44a1040a3c28e4989f24a5efa87f0c83b519
EliBrank <eliasbrinkman@gmail.com>
2024-01-25 20:19:11 +0000

    work through issues with print triangle func

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_triangle - draws right triangle of #s
+ * @size: side length of triangle
+ */
+
+void print_triangle(int size)
+{
+	int h = 1;
+	int pound;
+	int spacer;
+
+	if (size > 0)
+	{
+		while (h <= size)
+		{
+			spacer = 1;
+			while (spacer <= (size - h))
+			{
+				_putchar('.');
+				spacer++;
+			}
+			pound = 1;
+			while (pound <= h)
+			{
+				_putchar('#');
+				pound++;
+			}
+			h++;
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
