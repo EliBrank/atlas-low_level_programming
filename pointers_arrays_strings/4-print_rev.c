c53c9e2e3cf297113f7733f1b7c8f20e6da8a3c4
EliBrank <eliasbrinkman@gmail.com>
2024-01-26 17:45:41 +0000

    update reverse str func

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: input string
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	while (len >= 0)
+	{
+		_putchar(*s);
+		s--;
+		len--;
+	}
+
+	_putchar('\n');
+}
