f5380bd029bd0790412da70b2efe055c745dd466
EliBrank <eliasbrinkman@gmail.com>
2024-01-27 00:50:05 +0000

    make reversal func overly complicated

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * rev_string - returns string in reverse
@@ -9,6 +10,9 @@ void rev_string(char *s)
 {
 	int len = 0;
 	char *end = s;
+	char *temp;
+	int i;
+	int j;
 
 	while (*end != '\0')
 	{
@@ -16,21 +20,24 @@ void rev_string(char *s)
 		end++;
 	}
 
-	char temp[len];
-	int i = len;
-	int j = 0;
+	temp = malloc(len);
+	i = len;
+	j = 0;
 
 	while (i--)
 	{
 		temp[j] = s[i];
 		j++;
 	}
+
 	temp[len] = '\0';
 	i = len;
 	j = 0;
+
 	while (i--)
 	{
 		s[j] = temp[j];
 		j++;
 	}
-
+	free(temp);
+}
