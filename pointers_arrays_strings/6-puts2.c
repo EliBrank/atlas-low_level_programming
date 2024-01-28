29f6741ffb152b9b26666d3fe4c1e3634390cec7
EliBrank <eliasbrinkman@gmail.com>
2024-01-28 03:36:56 +0000

    update print evens func

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,5 +7,22 @@
 
 void puts2(char *str)
 {
+	int len = 0;
+	int i;
 
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+	str = str - len;
+
+	for (i = 0; i < len; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
 }
