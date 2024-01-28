bedfe71fce58022c31b0652bab8d46a32cd1655b
EliBrank <eliasbrinkman@gmail.com>
2024-01-28 04:00:13 +0000

    update half puts func

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * puts_half - prints the second half of a string
@@ -7,5 +8,30 @@
 
 void puts_half(char *str)
 {
+	int len = 0;
+	int pos;
+	int i;
 
+	while (*str != '\0')
+	{
+		str++;
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		pos = len / 2;
+	}
+	else
+	{
+		pos = (len - 1) / 2;
+	}
+
+	str = str - pos;
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
 }
