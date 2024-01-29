4f4f6aa8417dd50425e105ec569528f2c09da6b9
EliBrank <eliasbrinkman@gmail.com>
2024-01-29 03:30:24 +0000

    try strcpy func config

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -1,14 +1,25 @@
 #include "main.h"
 
 /**
- * _strcpy - 
+ * _strcpy - copies string to destination buffer
  * @dest: pointer to buffer
- * @src: input strilng to copy
+ * @src: input string to copy
  *
  * Return: pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
+	char *temp = dest;
+	int len = 0;
 
+	while (*src != '\0')
+	{
+		*dest++ = *src++;
+		len++;
+	}
+	*dest = '\0';
+	dest -= len;
+
+	return (dest);
 }
