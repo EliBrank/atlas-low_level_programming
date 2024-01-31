7882a5887842984eb360573ad3f957af64286cca
EliBrank <eliasbrinkman@gmail.com>
2024-01-31 20:48:46 +0000

    update strncat func

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: first string to be concatenated
+ * @src: second string to be concatenated
+ * @n: cut-off in bytes for src string
+ *
+ * Return: string made up of two input strings
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+	int len = 0;
+
+	while (dest[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; src[i] != '\0'; i++, len++)
+	{
+		dest[len] = src[i];
+		if (i * sizeof(src[0]) == n)
+		{
+			break;
+		}
+	}
+
+	dest[len] = '\0';
+
+	return (dest);
+}
