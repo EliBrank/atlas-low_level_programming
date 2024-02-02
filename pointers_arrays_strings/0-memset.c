f9668c055ec040a3e5665c1620f37972f4bdc5ed
EliBrank <eliasbrinkman@gmail.com>
2024-02-02 19:16:09 +0000

    do initial setup for functions, start on memset func

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ * @s: starting address to overwrite
+ * @b: constant byte used to fill memory
+ * @n: how many bytes of memory to fill
+ *
+ * Return: pointer to memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
