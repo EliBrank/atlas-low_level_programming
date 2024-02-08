95380f1e89c841166f454954f59dcecbcc039ef2
EliBrank <eliasbrinkman@gmail.com>
2024-02-08 19:23:55 +0000

    work on prime number recursion func

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,21 +1,41 @@
 #include "main.h"
 
 /**
- * is_prime_number - finds whether input is prime number
+ * prime_checker - checks whether input n is prime
+ * @n: number to check if prime
+ * @i: increments to test if number is prime
+ *
+ * Return: 1 if number is prime, else 0
+ */
+
+int prime_checker(int n, int i)
+{
+	if (i <= n / 2)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+		return (prime_checker(n, (i + 1)));
+	}
+	return (1);
+}
+
+/**
+ * is_prime_number - passes number to prime_checker to check if prime
  * @n: number to check if prime
  *
- * Return: 1 if number is prime, otherwise 0
+ * Return: 1 if number is prime, else 0
  */
 
 int is_prime_number(int n)
 {
-	if (n <= 0)
+	if (n <= 1)
 	{
 		return (0);
 	}
-	else if (n == 1)
+	else
 	{
-		return (1);
+		return (prime_checker(n, 2));
 	}
-	else (
 }
