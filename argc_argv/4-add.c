bc4d5f097eb95391c6e2daf69d72d64b470c1476
EliBrank <eliasbrinkman@gmail.com>
2024-02-09 20:14:12 +0000

    $commit_message

---
+++ argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - adds together positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 if success or no nums, 1 if NaN symbol 
+ */
+
+int main(int argc, char **argv)
+{
+	int sum;
+	int i;
+	int j;
+	int numLen;
+
+	if (argc == 1)
+	{
+		putchar('\n');
+		return (0);
+	}
+
+	for (i = 0; i < argc; i++)
+	{
+		numLen = strlen(argv[i]);
+		for (j = 0; j < numLen; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
