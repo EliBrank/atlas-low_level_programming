9d63f337ef1944a5a43b20d5492a8ebaab21b2dc
EliBrank <eliasbrinkman@gmail.com>
2024-02-14 21:51:30 +0000

    adjust positioning

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,14 +15,17 @@ char *str_concat(char *s1, char *s2)
 	char *catStr;
 	int i;
 	int j;
-	int lenS1 = strlen(s1);
-	int lenS2 = strlen(s2);
+	int lenS1;
+	int lenS2;
 
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
+
+	lenS1 = strlen(s1);
+	lenS2 = strlen(s2);
 
 	catStr = malloc(sizeof(char) * (lenS1 + lenS2) + 1);
 
