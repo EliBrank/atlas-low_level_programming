1146c47e1dd8a2c834d1040705e9f9434f2bb3d8
EliBrank <eliasbrinkman@gmail.com>
2024-02-14 21:28:02 +0000

    first push on str concat func

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string to concatenate
+ * @s2: second string to concatenate
+ *
+ * Return: concatenated string, NULL on failure
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *catStr;
+	int i;
+	int j;
+	int lenS1 = strlen(s1);
+	int lenS2 = strlen(s2);
+	
+	catStr = malloc(sizeof(char) * (lenS1 + lenS2) + 1);
+
+	if (catStr == NULL)
+	{
+		return (NULL);
+	}
+	
+	/* loop through first string */
+	for (i = 0; i < lenS1; i++)
+	{
+		catStr[i] = s1[i];
+	}
+	
+	/* loop through second string */
+	for (j = 0; j < lenS2; j++, i++)
+	{
+		catStr[i] = s2[j];
+	}
+
+	catStr[i] = '\0';
+
+	return (catStr);
+}
