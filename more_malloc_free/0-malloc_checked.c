0ec2cf747273b7adf5c418a70b9ea9dea5cabbc1
EliBrank <eliasbrinkman@gmail.com>
2024-02-17 23:23:52 +0000

    work on maclloc check func

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -2,11 +2,22 @@
 #include <stdlib.h>
 
 /**
- * malloc_checked - allocates memory for unsigned int
- * @b: int to allocate memory for
+ * malloc_checked - allocates memory using malloc for input
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to allocated data, 98 if failure
  */
 
 void *malloc_checked(unsigned int b)
 {
+	void *data;
 
+	data = malloc(b);
+
+	if (data == NULL)
+	{
+		exit (98);
+	}
+
+	return (data);
 }
