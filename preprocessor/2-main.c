fb448f8108b15fe07ae2a0360fcdd54a355cc253
EliBrank <eliasbrinkman@gmail.com>
2024-02-19 04:40:25 +0000

    make filename print func

---
+++ preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints filename
+ *
+ * Return: 0 (always success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
