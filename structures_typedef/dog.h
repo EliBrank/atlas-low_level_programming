4f4bf1062bf0693cb1a1bf499851d7047cc70a71
EliBrank <eliasbrinkman@gmail.com>
2024-02-22 02:19:48 +0000

    fix betty style

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,18 @@
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - simple dog structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog owner
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+#endif
