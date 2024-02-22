f9ef9b09110d8254d04cb265c11ded3924bca41a
EliBrank <eliasbrinkman@gmail.com>
2024-02-22 19:52:02 +0000

    try free dog func

--- structures_typedef/main.c
+++ structures_typedef/main.c
@@ -1,5 +1,5 @@
-#include "dog.h"
 #include <stdio.h>
+#include "dog.h"
 
 /**
  * main - check the code
@@ -12,5 +12,6 @@ int main(void)
 
     my_dog = new_dog("Poppy", 3.5, "Bob");
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+    free_dog(my_dog);
     return (0);
 }
