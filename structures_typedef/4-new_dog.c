c2edee42a07a6e8e18c65b39d18988579dc42d4e
EliBrank <eliasbrinkman@gmail.com>
2024-02-22 19:04:59 +0000

    include strdup in file

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -3,6 +3,44 @@
 #include <stdio.h>
 
 /**
+ * _strdup - duplicates a string
+ * @str: string to duplicate
+ *
+ * Return: duplicated string
+ */
+
+char *_strdup(char *str)
+{
+	char *dupStr;
+	int i;
+	int len;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for(len = 0; str[len] != '\0'; len++);
+	str -= len;
+
+	dupStr = malloc(sizeof(char) * len + 1);
+
+	if (dupStr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dupStr[i] = str[i];
+	}
+
+	dupStr[i] = '\0';
+
+	return (dupStr);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: new dog name
  * @age: new dog age
