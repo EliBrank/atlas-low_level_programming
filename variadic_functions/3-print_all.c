2e5842fea9ea77639edca92d3f880657778d2595
EliBrank <eliasbrinkman@gmail.com>
2024-02-26 20:54:48 +0000

    break up function into 2 parts

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,51 +4,67 @@
 #include <stdio.h>
 
 /**
- * print_all - prints all string, int args, then newline
+ * print_args - prints input args based on format
+ * @format: string of input types
+ * @args: var number of args
+ */
+
+void print_args(const char *format, va_list args)
+{
+	int len = 0, i = 0, noMatch = 0;
+
+	while (format != NULL && format[len] != '\0')
+			len++;
+		while (format != NULL && i < len)
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%c", va_arg(args, int));
+					break;
+				case 'i':
+					printf("%d", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f", va_arg(args, double));
+					break;
+				case 's':
+					{
+						char *str = va_arg(args, char*);
+
+						if (str == NULL)
+						{
+							printf("(nil)");
+							break;
+						}
+						printf("%s", str);
+						break;
+					}
+				default:
+					noMatch = 1;
+			}
+			if (i < (len - 1) && (noMatch != 1))
+				printf(", ");
+			noMatch = 0;
+			i++;
+		}
+}
+
+
+
+/**
+ * print_all - initializes printing args, then prints newline
  * @format: string specifying datatypes to be printed, in order
  */
 
 void print_all(const char * const format, ...)
 {
-	int len = 0, i = 0, noMatch = 0;
 	va_list args;
 
-	while (format != NULL && format[len] != '\0')
-		len++;
 	va_start(args, format);
-	while (format != NULL && i < len)
-	{
-		switch (format[i])
-		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				{
-					char *str = va_arg(args, char*);
 
-					if (str == NULL)
-					{
-						printf("(nil)");
-						break;
-					}
-					printf("%s", str);
-					break;
-				}
-			default:
-				noMatch = 1;
-		}
-		if (i < (len - 1) && (noMatch != 1))
-			printf(", ");
-		noMatch = 0;
-		i++;
-	}
+	print_args(format, args);
+
 	va_end(args);
 	putchar('\n');
 }
