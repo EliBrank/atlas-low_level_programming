badcae9b16cb403c2be5d60275376438dc2da6f3
EliBrank <eliasbrinkman@gmail.com>
2024-02-26 00:31:03 +0000

    save progress on print all func

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -5,35 +5,47 @@
 
 /**
  * print_all - prints all string, int args, then newline
+ * @format: string specifying datatypes to be printed, in order
  */
 
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i;
-	char *x;
+	int i;
+	int len;
+	const char *f;
+	char *separator = ", ";
 
-	va_start(args, n);
-
-	for (i = 0; i < n; i++)
+	f = format;
+	while (*f != '\0')
+		f++;
+	len = f - format;
+	va_start(args, format);
+	i = 0;
+	while (i < len)
 	{
-		x = va_arg(args, char *);
-		if (x == NULL)
+		switch (format[i])
 		{
-			printf("(nil)");
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				if (va_arg(args, char*) == NULL)
+					printf("(nil)");
+				else
+					printf("%s", va_arg(args, char *));
+				break;
 		}
-		else
-		{
-			printf("%s", x);
-		}
-
-		if (separator != NULL && i < (n - 1))
-		{
+		if (separator != NULL && i < (len - 1))
 			printf("%s", separator);
-		}
+		i++;
 	}
-
 	va_end(args);
-
 	putchar('\n');
 }
