5cf1b734fa324f27b780adcdaa2b207e73009b3e
EliBrank <eliasbrinkman@gmail.com>
2024-02-26 20:57:56 +0000

    rework tabs, make betty compliant

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -14,40 +14,40 @@ void print_args(const char *format, va_list args)
 	int len = 0, i = 0, noMatch = 0;
 
 	while (format != NULL && format[len] != '\0')
-			len++;
-		while (format != NULL && i < len)
+		len++;
+	while (format != NULL && i < len)
+	{
+		switch (format[i])
 		{
-			switch (format[i])
-			{
-				case 'c':
-					printf("%c", va_arg(args, int));
-					break;
-				case 'i':
-					printf("%d", va_arg(args, int));
-					break;
-				case 'f':
-					printf("%f", va_arg(args, double));
-					break;
-				case 's':
-					{
-						char *str = va_arg(args, char*);
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				{
+					char *str = va_arg(args, char*);
 
-						if (str == NULL)
-						{
-							printf("(nil)");
-							break;
-						}
-						printf("%s", str);
+					if (str == NULL)
+					{
+						printf("(nil)");
 						break;
 					}
-				default:
-					noMatch = 1;
-			}
-			if (i < (len - 1) && (noMatch != 1))
-				printf(", ");
-			noMatch = 0;
-			i++;
+					printf("%s", str);
+					break;
+				}
+			default:
+				noMatch = 1;
 		}
+		if (i < (len - 1) && (noMatch != 1))
+			printf(", ");
+		noMatch = 0;
+		i++;
+	}
 }
 
 
