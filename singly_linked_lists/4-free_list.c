b7787b2399d7d471edf236acac03b9455d916afa
EliBrank <eliasbrinkman@gmail.com>
2024-03-06 23:12:45 +0000

    try different free arrangements to free linked list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,5 +1,6 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * free_list - frees a linked list
@@ -8,5 +9,13 @@
 
 void free_list(list_t *head)
 {
+	list_t *tmp = NULL;
 
+	while (head != NULL)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp->str);
+		free(tmp);
+	}
 }
