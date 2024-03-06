19a5a7df6723ce7f75cd58247522584c42ad0472
EliBrank <eliasbrinkman@gmail.com>
2024-03-06 22:49:22 +0000

    include _strlen within append func

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -48,3 +48,22 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	return (new);
 }
+
+/**
+ * _strlen - returns the length of a string
+ * @s: input string
+ *
+ * Return: string length
+ */
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
