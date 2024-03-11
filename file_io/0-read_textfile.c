a1cb1854736fbec2ea877228b77cf4f2a4fd37f6
EliBrank <eliasbrinkman@gmail.com>
2024-03-11 21:15:40 +0000

    work read/print text file func

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -12,5 +13,46 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd, nRead, nWrite;
+	char *buf;
 
+	buf = malloc(sizeof(char) * letters + 1);
+	if (buf == NULL)
+		return (0);
+
+	if (filename == NULL)
+	{
+		free(buf);
+		return (0);
+	}
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	nRead = read(fd, buf, letters);
+	if (nRead == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	nWrite = write(STDOUT_FILENO, buf, nRead);
+	if (nWrite == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	if (close(fd) == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	free(buf);
+	return (nWrite);
 }
