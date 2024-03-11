73f021babacf573a3a4469b376a7417806c01dd5
EliBrank <eliasbrinkman@gmail.com>
2024-03-11 04:07:22 +0000

    configure set bit func

--- bit_manipulation/3-set_bit.c
+++ bit_manipulation/3-set_bit.c
@@ -4,12 +4,21 @@
 /**
  * set_bit - sets value of a bit to 1 at given index
  * @n: int to change
- * index: index of int to set (in binary)
+ * @index: index of int to set (in binary)
  *
  * Return: 1 if success, -1 if error
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int bitCheck = 1;
+	unsigned int numBits = sizeof(unsigned long int) * 8;
 
+	bitCheck = bitCheck << index;
+
+	if (index > (numBits - 1))
+		return (-1);
+
+	*n = (*n | bitCheck);
+	return (1);
 }
