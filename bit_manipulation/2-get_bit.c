4244af949bd3e12d4a07d2cd18b9d9416aa2a305
EliBrank <eliasbrinkman@gmail.com>
2024-03-11 03:53:55 +0000

    make preliminary pass of get bit func

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -11,23 +11,11 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int i;
-	int bit = 0;
-	int numBits_ulint = sizeof(unsigned long int) * 8;
+	unsigned long int bitCheck = 1;
 
-	for (i = 0; i < numBits_ulint; i++)
-	{
-		bit = n & 1;
-		binaryNum = (binaryNum << 1) | bit;
-		n >>= 1;
-	}
-
-	for (i = 0; i < numBits_int; i++)
-	{
-		bit = (binaryNum >> i) & 1;
-		if (bit == 1)
-			printLeader = 1;
-		if (printLeader == 1)
-			_putchar (bit + '0');
-	}
+	bitCheck = bitCheck << index;
+	if ((n & bitCheck) == bitCheck)
+		return (1);
+	else
+		return (0);
 }
