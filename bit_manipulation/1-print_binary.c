095e9cae2c2add470d43a9c709851af61415d753
EliBrank <eliasbrinkman@gmail.com>
2024-03-11 02:53:30 +0000

    fix sizeof measurement

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -8,35 +8,16 @@
 
 void print_binary(unsigned long int n)
 {
-	/* example is 12, 1100 */
 	int i;
-	unsigned long int bit = 0;
+	int bit = 0;
 	int printLeader = 0;
-	unsigned long int numBits = sizeof(int) * 8;
+	int numBits = sizeof(unsigned long int) * 8;
 
 	if (n == 0)
 	{
 		_putchar('0');
 		return;
 	}
-
-	/*
-	for (i = 0; i < numBits; i++)
-	{
-		bit = n & 1;
-		binaryNum = (binaryNum << 1) | bit;
-		n >>= 1;
-	}
-
-	for (i = 0; i < numBits_ulint; i++)
-	{
-		bit = (binaryNum >> i) & 1;
-		if (bit == 1)
-			printLeader = 1;
-		if (printLeader == 1)
-			_putchar (bit + '0');
-	}
-	*/
 
 	for (i = numBits - 1; i >= 0; i--)
 	{
