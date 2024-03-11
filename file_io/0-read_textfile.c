756a518fb0185a99e3d000731bd7bd1b88b36fc2
EliBrank <eliasbrinkman@gmail.com>
2024-03-11 21:17:29 +0000

    reduce lines to fit betty compliance

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -19,40 +19,34 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buf = malloc(sizeof(char) * letters + 1);
 	if (buf == NULL)
 		return (0);
-
 	if (filename == NULL)
 	{
 		free(buf);
 		return (0);
 	}
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		free(buf);
 		return (0);
 	}
-
 	nRead = read(fd, buf, letters);
 	if (nRead == -1)
 	{
 		free(buf);
 		return (0);
 	}
-
 	nWrite = write(STDOUT_FILENO, buf, nRead);
 	if (nWrite == -1)
 	{
 		free(buf);
 		return (0);
 	}
-
 	if (close(fd) == -1)
 	{
 		free(buf);
 		return (0);
 	}
-
 	free(buf);
 	return (nWrite);
 }
