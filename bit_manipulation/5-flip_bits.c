2785314c5ac2fde9db0a5e8e23aec58d9b99949b
EliBrank <eliasbrinkman@gmail.com>
2024-03-11 06:10:42 +0000

    work more on flip bits func

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -11,5 +11,19 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	int numBits = sizeof(unsigned long int) * 8;
+	int bitsToChange = 0;
+	int bitEval = 0;
+	int i;
 
+
+	for (i = numBits - 1; i >= 0; i--)
+	{
+		bitEval = ((n >> i) & 1) ^ ((m >> i) & 1);
+
+		if (bitEval == 1)
+			bitsToChange += 1;
+	}
+
+	return (bitsToChange);
 }
