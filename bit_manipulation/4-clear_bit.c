fc4d475196518a1f8c323a0cbbc3bf605ee57f90
EliBrank <eliasbrinkman@gmail.com>
2024-03-11 05:40:18 +0000

    rework, simplify clear bit func

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -13,33 +13,16 @@
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int bitCheck = 1;
-	unsigned long int nFlip = ~(*n);
 	unsigned int numBits = sizeof(unsigned long int) * 8;
-	int sigBit = 0;
-	int bit = 0;
-	int i;
 
 	if (index > (numBits - 1))
 		return (-1);
 
 	bitCheck = bitCheck << index;
 
-	nFlip = (nFlip | bitCheck);
-	printf("%lu\n", nFlip);
-
-	for (i = numBits - 1; i >= 0; i--)
-	{
-		bit = ((*n) >> i) & 1;
-		if (bit == 1)
-		{
-			sigBit = numBits - i;
-			break;
-		}
-	}
-
-	printf("%lu\n", nFlip);
-
-	*n = *n & nFlip;
+	*n = ~(*n);
+	*n = ((*n) | bitCheck);
+	*n = ~(*n);
 
 	return (1);
 }
