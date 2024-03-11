75dec4b6d44ff8439db82f0144bf622193cd98c5
EliBrank <eliasbrinkman@gmail.com>
2024-03-11 03:57:48 +0000

    try check for out of range index

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -12,10 +12,14 @@
 int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int bitCheck = 1;
+	unsigned int numBits = sizeof(unsigned long int) * 8;
 
 	bitCheck = bitCheck << index;
+
+	if (index > (numBits - 1))
+		return (-1);
 	if ((n & bitCheck) == bitCheck)
 		return (1);
-	else
-		return (0);
+
+	return (0);
 }
