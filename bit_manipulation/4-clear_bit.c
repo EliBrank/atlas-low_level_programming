62826d76652aef665f7d82636a159fdf22802c43
EliBrank <eliasbrinkman@gmail.com>
2024-03-11 05:05:19 +0000

    work on setting up clear bit func

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * clear_bit - sets value of a bit to 0 at given index
@@ -11,5 +12,32 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned long int bitCheck = 1;
+	unsigned long int nFlip = ~(*n);
+	unsigned int numBits = sizeof(unsigned long int) * 8;
+	int sigBit = 0;
+	int bit = 0;
+	int i;
 
+	if (index > (numBits - 1))
+		return (-1);
+
+	bitCheck = bitCheck << index;
+
+	nFlip = (nFlip | bitCheck);
+
+	for (i = numBits - 1; i >= 0; i--)
+	{
+		bit = ((*n) >> i) & 1;
+		if (bit == 1)
+		{
+			sigBit = i;
+			break;
+		}
+	}
+	printf("%d\n", sigBit);
+
+	*n = *n ^ nFlip;
+
+	return (1);
 }
