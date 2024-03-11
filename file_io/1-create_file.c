e9756823d51d131fceeb1a8440e219a94df05e21
EliBrank <eliasbrinkman@gmail.com>
2024-03-11 23:56:03 +0000

    work on append func

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -29,6 +29,9 @@ int create_file(const char *filename, char *text_content)
 	if (nWrite == -1)
 		return (-1);
 
+	if (close(fd) == -1)
+		return (-1);
+
 	return (1);
 }
 
--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -9,8 +9,50 @@
  *
  * Return: 1 on success, -1 on failure
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd, nWrite, contentLen;
 
+	contentLen = _strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	if (contentLen != 0)
+		nWrite = write(fd, text_content, contentLen);
+
+	if (nWrite == -1)
+		return (-1);
+
+	if (close(fd) == -1)
+		return (-1);
+
+	return (1);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: input string
+ *
+ * Return: string length
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	if (s == NULL)
+		return (0);
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
 }
