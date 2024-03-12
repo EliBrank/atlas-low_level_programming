e43bfdd1845fb754b98501b449c8fbafab63bf4a
EliBrank <eliasbrinkman@gmail.com>
2024-03-12 01:36:54 +0000

    work through most of the issues with cp function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -1,38 +1,62 @@
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 #define BUFFER_SIZE 1024
 
 int main(int argc, char *argv[])
 {
+	int fd_to, fd_from, nRead, nWrite;
+	char buf[BUFFER_SIZE];
+	char *file_from, *file_to;
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	char *file_from = argv[1];
-	char *file_to = argv[2];
-	int fd, nRead, nWrite;
-	char buf[BUFFER_SIZE];
+	file_from = argv[1];
+	file_to = argv[2];
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprint(STDERR_FILENO,
+		dprintf(STDERR_FILENO,
 		"Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 
-	fd_to = open(file_to, O_WRONLY | O_APPEND | O_TRUNC, 0664);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
-		dprint(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
 
-	while ((nRead = read(fd, buf, BUFFER_SIZE)) > 0)
+	while ((nRead = read(fd_from, buf, BUFFER_SIZE)) > 0)
 	{
-		nWrite = write(
+		nWrite = write(fd_to, buf, nRead);
+		if (nWrite == -1)
+		{
+			dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+
+	if (close(fd_from) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	if (close(fd_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
+
+	return (0);
 }
