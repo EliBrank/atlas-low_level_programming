0fce6f4249cb3db0d29809736676f84ed9a1f722
EliBrank <eliasbrinkman@gmail.com>
2024-03-12 01:02:22 +0000

    save progress working on custom cp func

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,10 +2,37 @@
 #include <fcntl.h>
 #include "main.h"
 
+#define BUFFER_SIZE 1024
+
 int main(int argc, char *argv[])
 {
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+
 	char *file_from = argv[1];
 	char *file_to = argv[2];
+	int fd, nRead, nWrite;
+	char buf[BUFFER_SIZE];
 
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		dprint(STDERR_FILENO,
+		"Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
 
+	fd_to = open(file_to, O_WRONLY | O_APPEND | O_TRUNC, 0664);
+	if (fd_to == -1)
+	{
+		dprint(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	while ((nRead = read(fd, buf, BUFFER_SIZE)) > 0)
+	{
+		nWrite = write(
 }
