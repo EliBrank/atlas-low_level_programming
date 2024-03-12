7cd5bea4748f69702c8d88772178bd48f7f771a9
EliBrank <eliasbrinkman@gmail.com>
2024-03-12 02:45:54 +0000

    fix problems with null check for head node

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -24,11 +24,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->n = n;
 	new->next = NULL;
 
-	if (*head == NULL)
-	{
-		*head = new;
-		return (*head);
-	}
+	if (head == NULL)
+		return (NULL);
 
 	if (idx == 0)
 		return (add_nodeint(head, n));
@@ -46,31 +43,4 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	new->next = tmp->next;
 	return (new);
-}
-
-/**
- * add_nodeint - prepends node to linked list
- * @head: current first element in list
- * @n: int to duplicate for prepended element
- *
- * Return: address of added element, NULL if failure
- */
-listint_t *add_nodeint(listint_t **head, const int n)
-{
-	listint_t *new = NULL;
-
-	if (head == NULL)
-		return (NULL);
-
-	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-		return (NULL);
-
-	new->n = n;
-	new->next = *head;
-
-	*head = new;
-
-	return (*head);
 }
