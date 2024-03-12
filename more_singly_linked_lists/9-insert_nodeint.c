f1bb1fb5cadb32574cfccd3e47d9c86438abab38
EliBrank <eliasbrinkman@gmail.com>
2024-03-12 02:49:13 +0000

    add back in brackets for larger if statement

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -28,9 +28,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	if (idx == 0)
+	{
 		new->next = *head;
 		*head = new;
 		return (*head);
+	}
 
 	tmp = *head;
 	i = 0;
