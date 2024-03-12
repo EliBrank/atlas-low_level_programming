5eabd63f26d954afde6fbe89cef64eda575da3af
EliBrank <eliasbrinkman@gmail.com>
2024-03-12 02:42:09 +0000

    make adjustments to return, error handling for insert node func

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -18,10 +18,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i;
 
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
 	new->n = n;
 	new->next = NULL;
 
-	if (*head == NULL && idx == 0)
+	if (*head == NULL)
 	{
 		*head = new;
 		return (*head);
@@ -42,7 +45,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 
 	new->next = tmp->next;
-	return (tmp);
+	return (new);
 }
 
 /**
