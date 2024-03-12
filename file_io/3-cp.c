6bdbce511586b736dde00adbe0301adeefce4537
EliBrank <eliasbrinkman@gmail.com>
2024-03-12 04:55:26 +0000

    try write without append flag

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -94,7 +94,7 @@ int open_to(char *file_to)
 {
 	int fd;
 
-	fd = open(file_to, O_WRONLY | O_CREAT | O_APPEND | O_TRUNC, 0664);
+	fd = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO,
