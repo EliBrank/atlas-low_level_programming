ec157337b0cb510062db8b5a7d1b93c17622cd1a
EliBrank <eliasbrinkman@gmail.com>
2024-03-14 21:42:02 +0000

    work through prepend func

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,20 +13,23 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
 
+	if (head == NULL)
+		return (NULL);
+
 	new = malloc(sizeof(dlistint_t));
 
 	if (new == NULL)
 		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
 		*head = new;
 		return (*head);
 	}
-
-	new->n = n;
-	new->prev = NULL;
-	new->next = NULL;
 
 	new->next = *head;
 	(*head)->prev = new;
--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,5 +11,35 @@
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
+	dlistint_t *new = NULL;
+	dlistint_t *tmp = NULL;
 
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+
+	tmp = *head;
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+
+	tmp->next = new;
+	new->prev = tmp;
+
+	return (new);
 }
