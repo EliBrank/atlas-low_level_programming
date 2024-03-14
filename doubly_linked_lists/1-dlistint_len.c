7827541c8664c0b9288c175b5a8bd539fb9310d8
EliBrank <eliasbrinkman@gmail.com>
2024-03-14 19:55:01 +0000

    rework print func into len func

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -1,4 +1,4 @@
-#include <stdiio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "lists.h"
 
@@ -10,5 +10,18 @@
  */
 size_t dlistint_len(const dlistint_t *h)
 {
+	int count = 0;
+	const dlistint_t *tmp = h;
+
+	if (h == NULL)
+		return (0);
+
+	while (tmp != NULL)
+	{
+		count++;
+		tmp = tmp->next;
+	}
+
+	return (count);
 
 }
