98840ec2ad7e12319b31ecdcd24f4a533aded22f
EliBrank <eliasbrinkman@gmail.com>
2024-03-14 23:30:02 +0000

    work more on insert node func

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,40 +12,43 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *tmp = NULL;
 	dlistint_t *new = NULL;
-	unsigned int i;
+	dlistint_t *tmp = NULL;
+	dlistint_t *tmp2 = NULL;
 
+	new = node_init(&new, n);
+	if (h == NULL)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		add_dnodeint(&h, n);
+	}
+
+	tmp = *h;
+	i = 0;
+	while (idx > 1)
+	{
+		tmp = tmp->next;
+		idx--;
+	}
+	tmp2 = tmp->next;
+	tmp->next = new;
+	tmp2->prev = new;
+	new->next = tmp2;
+	new->prev = tmp;
+	return (new);
+}
+
+dlistint_t *node_init(dlistint_t **new, int n)
+{
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
-	if (h == NULL)
-		return (NULL);
-	if (idx == 0)
-	{
-		(*h)->prev = new;
-		new->next = *h;
-		*h = new;
-		return (*h);
-	}
-	tmp = *h;
-	i = 0;
-	while (tmp != NULL && i < (idx - 1))
-	{
-		tmp = tmp->next;
-		i++;
-	}
-	if (tmp == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
-	new->prev = tmp->prev;
-	tmp->prev->next = new;
-	new->next = tmp;
-	tmp->prev = new;
-	return (new);
+
+	return (*new);
 }
