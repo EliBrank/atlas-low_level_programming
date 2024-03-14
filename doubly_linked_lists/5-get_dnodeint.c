e5ccdf1d4ee5d0e031d6c743e18c91b7d8e8063c
EliBrank <eliasbrinkman@gmail.com>
2024-03-14 22:00:51 +0000

    try adapting singly linked list get node func for doubly linked lists

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -11,5 +11,17 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
+	dlistint_t *tmp = head;
+	unsigned int i;
 
+	i = 0;
+	while (tmp != NULL)
+	{
+		if (i == index)
+			return tmp;
+		i++;
+		tmp = tmp->next;
+	}
+
+	return (NULL);
 }
