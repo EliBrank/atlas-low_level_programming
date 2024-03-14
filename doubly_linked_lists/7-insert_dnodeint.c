ab20eb9f0b45f61566f2dd041afbeb41b9fcd251
EliBrank <eliasbrinkman@gmail.com>
2024-03-14 22:30:17 +0000

    work on adapting insert node at index (singly linked list) to doubly linked list version

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -12,5 +12,40 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
+	dlistint_t *tmp = NULL;
+	dlistint_t *new = NULL;
+	unsigned int i;
 
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	if (h == NULL)
+		return (NULL);
+	if (idx == 0)
+	{
+		(*h)->prev = new;
+		new->next = *h;
+		*h = new;
+		return (*h);
+	}
+	tmp = *h;
+	i = 0;
+	while (tmp != NULL && i < (idx - 1))
+	{
+		tmp = tmp->next;
+		i++;
+	}
+	if (tmp == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->prev = tmp->prev;
+	tmp->prev->next = new;
+	new->next = tmp;
+	tmp->prev = new;
+	return (new);
 }
