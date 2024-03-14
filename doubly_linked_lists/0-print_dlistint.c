74765c357d831c83ab4d24ccb822548ce9e15c0a
EliBrank <eliasbrinkman@gmail.com>
2024-03-14 19:48:49 +0000

    make print function for doubly linked lists

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -10,5 +10,18 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
+	int count = 0;
+	const dlistint_t *tmp = h;
+	
+	if (h == NULL)
+		return (0);
 
+	while (tmp != NULL)
+	{
+		count++;
+		printf("%d\n", tmp->n);
+		tmp = tmp->next;
+	}
+
+	return (count);
 }
