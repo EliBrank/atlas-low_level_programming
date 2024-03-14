5661080a45a331785dd0255526658af502148ce8
EliBrank <eliasbrinkman@gmail.com>
2024-03-14 20:11:20 +0000

    test prepend func

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,5 +11,24 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *new;
 
+	if (head == NULL)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->prev = NULL;
+	new->next = NULL;
+
+	new->next = *head;
+	(*head)->prev = new;
+
+	*head = new;
+
+	return (*head);
 }
