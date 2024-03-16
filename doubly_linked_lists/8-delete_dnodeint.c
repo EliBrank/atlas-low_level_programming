771289b47d8cb844e5bb63ca7dfb91fafe2bb53f
EliBrank <eliasbrinkman@gmail.com>
2024-03-16 02:35:58 +0000

    revert to previous github repo, update delete node func

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -11,5 +11,41 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
+        dlistint_t *tmp = NULL;
+        dlistint_t *follow = NULL;
 
+        if (*head == NULL)
+        {
+                return (-1);
+        }
+
+        tmp = *head;
+
+        if (index == 0)
+        {
+                *head = tmp->next;
+                free(tmp);
+                return (1);
+        }
+
+        while (index > 1)
+        {
+                tmp = tmp->next;
+                index--;
+        }
+
+        if (tmp == NULL || tmp->next == NULL)
+                return (-1);
+
+        if (tmp->next->next == NULL)
+                free(tmp->next);
+        else
+        {
+        follow = tmp->next->next;
+        free(tmp->next);
+        tmp->next = follow;
+        follow->prev = tmp;
+        }
+
+        return (1);
 }
