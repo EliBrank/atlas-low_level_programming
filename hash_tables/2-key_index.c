810ff0b653940f944fc7a2dfca9863a02db17432
EliBrank <eliasbrinkman@gmail.com>
2024-03-17 20:50:31 +0000

    add in missing return value for betty documentation

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -4,6 +4,8 @@
  * key_index - gets index of a key
  * @key: the key
  * @size: size of the array of the hash table
+ *
+ * Return: index of key
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
