3559e6309d2482b3c96712484ad7b3120d17893c
EliBrank <eliasbrinkman@gmail.com>
2024-03-17 22:00:45 +0000

    test hash table set func

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,5 +11,21 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	hash_node_t *new;
 
+	new = malloc(sizeof(hash_node_t));
+
+	if (key == NULL || new == NULL)
+		return (0);
+
+	index = key_index(key, 1024);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = ht->array[index];
+
+	ht->array[index] = new;
+
+	return (1);
 }
