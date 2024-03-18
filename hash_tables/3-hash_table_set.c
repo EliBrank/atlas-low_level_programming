6307a1db22ef129c9ac8e00f2a959c41ba22c8c9
EliBrank <eliasbrinkman@gmail.com>
2024-03-18 01:49:58 +0000

    test adding check for collisions

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new;
+	hash_node_t *new, *tmp;
 
 	new = malloc(sizeof(hash_node_t));
 
@@ -21,10 +21,22 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	index = key_index((unsigned char *)key, ht->size);
 
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+
 	new->key = strdup(key);
 	new->value = strdup(value);
-	new->next = ht->array[index];
 
+	new->next = ht->array[index];
 	ht->array[index] = new;
 
 	return (1);
