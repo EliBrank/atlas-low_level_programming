9bafbc9d9ca7ca8025f1d2acb534c6592fc4d588
EliBrank <eliasbrinkman@gmail.com>
2024-03-18 04:34:39 +0000

    work out how to print key value pairs in hash table

--- hash_tables/5-hash_table_print.c
+++ hash_tables/5-hash_table_print.c
@@ -7,5 +7,27 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *tmp;
+	unsigned int i;
+	bool firstPrint = true;
 
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			tmp = ht->array[i];
+			while (tmp != NULL)
+			{
+				if (!firstPrint)
+					printf(", ");
+				printf("\'%s\': \'%s\'", tmp->key, tmp->value);
+				tmp = tmp->next;
+				firstPrint = false;
+			}
+		}
+	}
+
+	printf("}\n");
 }
