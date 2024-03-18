e646fc680b007917ba1340ace69188a254d01768
EliBrank <eliasbrinkman@gmail.com>
2024-03-18 06:41:30 +0000

    work on function for freeing/deleting hash table

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -7,5 +7,28 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i;
+	hash_node_t *current;
+	hash_node_t *tmp;
 
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			tmp = current;
+			current = current->next;
+
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+
+	free(ht->array);
+
+	free(ht);
 }
