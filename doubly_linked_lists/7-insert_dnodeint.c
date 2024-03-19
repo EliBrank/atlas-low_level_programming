8749db7b4cceb0eff311922f2833196fa51fd2e5
EliBrank <eliasbrinkman@gmail.com>
2024-03-19 20:13:51 +0000

    work on playlist challenge

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,14 +16,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp = NULL;
 	dlistint_t *tmp2 = NULL;
 
+	if (h == NULL)
+		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	new->prev = NULL;
-	if (h == NULL)
-		return (NULL);
 	if (idx == 0)
 	{
 		add_dnodeint(h, n);
